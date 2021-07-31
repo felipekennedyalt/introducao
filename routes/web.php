9d159dcb00d91b583ae2af9ecc0b9caf8e31e092
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-07-31 09:20:41 -0300

    Laravel 8 - Aula 06 - Criando um Controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', HomeController::class {
     return view('welcome');
 });
 
