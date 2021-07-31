3f986517bea079ffcaecbcff1d669eb95e035c04
Felipe Kennedy <felipekennedyfvs2019@gmail.com>
2021-07-31 09:14:19 -0300

    Laravel 8 - Aula 05 - Parametros nas rotas

--- routes/web.php
+++ routes/web.php
@@ -17,10 +17,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('produtos/{nome}', function ($nome) {
-    return "Usuário e o seu id é $nome";
+Route::get('produtos/inserir', function () {
+    return "Página de inserir produto";
+});
+//para passar um parametro opcional onde ele não obrigatoriamente está presente eu utilizo "?" no fim do parametro para indicar como opcional, e no método passo sua variável como nulo
+Route::get('produtos/{nome}/{valor?}', function ($nome, $valor = null) {
+    //se valor existe
+    if($valor){
+        return "O nome do produto é $nome e seu valor é $valor";
+    }else{
+        return "O nome do produto é $nome";
+    }
+    
 });
 
 Route::get('users', function () {
     return 'usuário';
 });
+
